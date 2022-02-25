6c931c56febedb79c89addf77402b20902db556b
davinci2405 <44964929+davinci2405@users.noreply.github.com>
2022-02-25 16:44:58 +0700

    fix gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8aad08e278ea6e13"; }
+/******/ 		__webpack_require__.h = function() { return "25f5f40885470d6a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
