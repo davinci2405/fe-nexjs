a4f624464daae3fc0f04ead0ed06ab4d86e605b2
davinci2405 <44964929+davinci2405@users.noreply.github.com>
2022-02-28 11:19:29 +0700

    fix server.js

--- server/index.ts
+++ server/index.ts
@@ -6,15 +6,16 @@ import routes from "./routes";
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
-const handler = routes.getRequestHandler(app);
-
+const handler = routes.getRequestHandler(app, ({ req, res, route, query }) => {
+  const queries = { routeName: route.name, ...query };
+  app.render(req, res, route.page, queries);
+});
 app.prepare().then(() => {
   const server = express();
   app.setAssetPrefix(process.env.STATIC_PATH || "");
   server.use(express.static(path.join(__dirname, "../public/static")));
-  server.all("*", (req, res) => handler(req, res));
+  server.get("*", (req, res) => handler(req, res));
   server.listen(port);
-  // eslint-disable-next-line no-console
   console.log(
     `> Server listening at http://localhost:${port} as ${dev ? "development" : process.env.NODE_ENV
     }`
